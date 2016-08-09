ad223efe9c6f595a2e326f47ae900696520957e6
yanghaodong <478214853@qq.com>
2016-08-09 16:13:55 +0800

    map

---
+++ app/src/main/java/com/androidNewLab/baiduMap/GsonService.java
@@ -0,0 +1,22 @@
+package com.androidNewLab.baiduMap;
+
+/**
+ * Created by Administrator on 2016/7/28.
+ */
+import com.google.gson.Gson; //导入google gson包
+
+public class GsonService {
+
+    public static <T> T parseJson(String jsonString, Class<T> clazz) {
+        T t = null;
+        try {
+            Gson gson = new Gson();
+            t = gson.fromJson(jsonString, clazz);
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println("解析json失败");
+        }
+        return t;
+
+    }
+}
